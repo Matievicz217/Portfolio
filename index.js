ab3a4146f97954dfe845ab0b7737805c40f650ed
Matievicz217 <binhomatievicz@hotmail.com>
2026-06-20 15:48:21 -0300

    Terminando o portfolio

---
+++ index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.header', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1500
+});
+sr.reveal('.introducao', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1500
+});
+sr.reveal('.experiencia', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1500
+});
+sr.reveal('.formacao', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1500
+});
+sr.reveal('.footer', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1500
+});
+
